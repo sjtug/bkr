554c6359fe17dec1196c0d186b58c8d32247724e
Nihiue <nihiue@gmail.com>
2014-07-03 23:15:26 +0800

    function fetchBookInfo( isbn , dataHandler )

--- app/javascript/bookinfo.js
+++ app/javascript/bookinfo.js
@@ -1,9 +1,9 @@
 var ISBN_BOOK_INFO_HANDLER;
 var fetchBookInfo=function(isbn,dataHandler)  
 {
-		ISBN_BOOK_INFO_HANDLER=dataHandler||function(data){console.log(data)};
-        var url ="http://api.douban.com/v2/book/isbn/"+isbn+"?callback=ISBN_BOOK_INFO_HANDLER";
-        var script = document.createElement('script');
-        script.setAttribute('src', url);
-        document.getElementsByTagName('head')[0].appendChild(script); 
+  ISBN_BOOK_INFO_HANDLER=dataHandler||function(data){console.log(data)};
+  var url ="http://api.douban.com/v2/book/isbn/"+isbn+"?callback=ISBN_BOOK_INFO_HANDLER";
+  var script = document.createElement('script');
+  script.setAttribute('src', url);
+  document.getElementsByTagName('head')[0].appendChild(script); 
 };

878637f3679c16c005ec7e34b2462c604345581c
Chase Zhang <yun.er.run@gmail.com>
2014-07-04 16:50:45 +0800

    change left panel style, add routers to logout

--- app/javascript/views/indexView.js
+++ app/javascript/views/indexView.js
@@ -2,15 +2,20 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
 
+var user = require('../user');
+
 var IndexView = Backbone.View.extend({
   $el: $('.view-main'),
   $leftPanel: $('.panel-left'),
+  leftPanelTemplate: _.template($('#tmpl-left-side-menu').html()),
   initialize: function(){
     this.$leftPanel.on('opened', this.leftPanelOpened);
     this.$leftPanel.on('closed', this.leftPanelClosed);
   },
   render: function(){
-    console.log('render');
+    var avatar = user.avatar();
+    var username = user.current().get("username");
+    this.$leftPanel.html(this.leftPanelTemplate({avatar:avatar, username:username}));
   },
   leftPanelOpened: function(){
     console.log('left panel opened');

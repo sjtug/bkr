e30aeda3927d2c050ba9d72a06ccf9b7256674fa
Phil Xiaojun Hu <phil@cnphil.com>
2014-07-05 20:47:52 +0800

    bookview action

--- app/javascript/views/bookView.js
+++ app/javascript/views/bookView.js
@@ -2,6 +2,7 @@ var $ = require("jquery");
 var _ = require("underscore");
 
 var Backbone = require("backbone");
+var App = require('../app');
 
 var BookView = Backbone.View.extend({
   template: _.template($('#tmpl-book-view').html()),
@@ -10,6 +11,19 @@ var BookView = Backbone.View.extend({
     var ts = this;
     bookmanage.fetchBookInfo(data.isbn, function(bookjson) {
       data.bookjson = bookjson;
+      var succ_cbk = function (result) {
+        App.app.hideIndicator();
+        App.app.alert("图书添加成功", "添加成功");
+      };
+      var fail_cbk = function (result) {
+        App.app.hideIndicator();
+        App.app.alert("图书添加失败", "添加失败");
+      };
+        
+      data.addThisBook = function() {
+        App.app.showIndicator();
+        bookmanage.addBook(data.isbn, bookjson, succ_cbk, fail_cbk);
+      };
       ts.$el.html(ts.template(data))
     });
   }

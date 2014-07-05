b22016907315422122f587214a3f08c3c5a5fe5f
Nihiue <nihiue@gmail.com>
2014-07-05 12:50:03 +0800

    finished bookmanage.js
    addBook = function(isbn,success_callback,error_callback)
    removeBook = function(bookobj,success_callback,error_callback)
    listNearBook = function(callback)
    listBookByUser = function(userObj,callback)
    listUserByBook = function(bookObj,callback)

--- app/javascript/bookmanage.js
+++ app/javascript/bookmanage.js
@@ -1,6 +1,42 @@
 var bookinfo = require("./bookinfo.js");
 var Book =  AV.Object.extend("book");
 var User = AV.User.current();
+
+var fakeBookList = function(isbn,locationArray)
+{
+  var error_callback=function(data){console.log(data)};
+  var success_callback=function(data){console.log(data)};
+  var queryUserBookCallback = function(result)
+  {
+    if(result){
+       error_callback("BOOK_ALREADY_EXISTS");
+       return;
+    }
+    var bookInfoCallback = function(infodata){
+      if(infodata && infodata.title){     
+          var newbook = new Book();
+          newbook.set("detail",infodata);
+          newbook.set("title",infodata.title);
+          newbook.set("author",infodata.author);
+          newbook.set("isbn",isbn);
+          newbook.set("owner",User);
+          newbook.set("location",new AV.GeoPoint({latitude:locationArray[0], longitude: locationArray[1]}));
+          newbook.save(null, { 
+            success: success_callback,
+            error: function(newbook, error) {
+              error_callback(error.description);   
+            }
+          });       
+      } else{
+        error_callback("BOOK_INFO_NOT_FOUND");
+        return;
+      }
+    }
+    bookinfo.fetchBookInfo(isbn,bookInfoCallback)
+  }
+  queryUserBook(isbn,queryUserBookCallback);
+}
+
 var addBook = function(isbn,success_callback,error_callback) {
   error_callback=error_callback||function(data){console.log(data)};
   var queryUserBookCallback = function(result)
@@ -19,7 +55,7 @@ var addBook = function(isbn,success_callback,error_callback) {
           newbook.set("isbn",isbn);
           newbook.set("owner",User);
           newbook.set("location",new AV.GeoPoint({latitude: location.coords.latitude, longitude: location.coords.longitude}));
-          newbook.save(null, {
+          newbook.save(null, { 
             success: success_callback,
             error: function(newbook, error) {
               error_callback(error.description);   
@@ -40,7 +76,7 @@ var addBook = function(isbn,success_callback,error_callback) {
 var queryUserBook = function(isbn,callback){  
   var query = new AV.Query(Book);
   query.equalTo("owner", User);
-  query.equalTo("isbn",isbn);
+  query.equalTo("isbn", isbn);
   query.find({
       success: function(results){
         if(results.length){
@@ -56,43 +92,79 @@ var queryUserBook = function(isbn,callback){
 }
 
 var removeBook = function(bookobj,success_callback,error_callback){
-  error_callback=error_callback||function(data){console.log(data)};
+  error_callback=error_callback || function(data){console.log(data)};
       bookobj.destroy({
         success:success_callback,
         error:error_callback
     });
 }
   
-
-var listUserBook = function(callback){
-  var query = new AV.Query(Book);
-  query.equalTo("owner", User);
-  query.find({
-      success: function(results){
-        callback(results);
-      },
-      error : function(err){
-        console.log(err);
-      }
-    });
-}
-var listNearByBook = function(callback){
+var listNearBook = function(callback){
   var locationCallback = function(location){
+    var filterCallback = function (results){
+      var symbol = {};
+      var res = [];
+      for (var idx in results){
+        var isbn = results[idx].get("isbn")
+        if(isbn && !symbol[isbn]){          
+            symbol[isbn] = true;
+            res.push(results[idx]);          
+        }
+      } 
+    callback(res);
+    }
     var currentPoint = new AV.GeoPoint({latitude: location.coords.latitude, longitude: location.coords.longitude});
     var query = new AV.Query(Book);
-    query.near("location", currentPoint);
-    query.limit(50);
+    query.withinKilometers("location", currentPoint,5);
+    query.limit(100);
+    query.include("owner");
     query.notEqualTo("owner", User);
     query.find({
-    success: callback
+    success: filterCallback
     });
   }
 navigator.geolocation.getCurrentPosition(locationCallback);
 }
 
 
+var listBookByUser = function(userObj,callback){
+  var query = new AV.Query(Book); 
+  query.equalTo("owner", userObj);
+  query.find({
+      success: function(results){
+        callback(results);
+      },
+      error: function(err){
+        console.log(err);
+      }
+    });
+}
+
+var listUserByBook = function(bookObj,callback)
+{
+  function queryCallback(results)
+  {   
+    var ownerList = [];
+    tt=results;
+    for (var idx in results){
+      ownerList.push(results[idx].get("owner"));
+    }
+    callback(ownerList);
+  }
+
+   var query = new AV.Query(Book);
+   query.include("owner");
+   query.select("owner");
+   query.equalTo("isbn",bookObj.get("isbn"));
+   query.find({
+    success: queryCallback
+    });
+}
+
+//exports.fakeBookList  = fakeBookList ;
+
 exports.addBook  = addBook ;
 exports.removeBook  = removeBook ;
-exports.queryUserBook  = queryUserBook ;
-exports.listNearByBook  = listNearByBook ;
-exports.listUserBook   = listUserBook  ;
+exports.listNearBook  = listNearBook ;
+exports.listBookByUser   = listBookByUser  ;
+exports.listUserByBook   = listUserByBook  ;

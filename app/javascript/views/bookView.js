6799ea175ac93bc47ee3b749b804197bb75aaa7a
Phil Xiaojun Hu <phil@cnphil.com>
2014-07-06 09:59:38 +0800

    new, refined bookview actions

--- app/javascript/views/bookView.js
+++ app/javascript/views/bookView.js
@@ -11,22 +11,28 @@ var BookView = Backbone.View.extend({
     var ts = this;
     bookmanage.fetchBookInfo(data.isbn, function(bookjson) {
       data.bookjson = bookjson;
-      var succ_cbk = function (result) {
-        App.app.hideIndicator();
-        App.app.alert("图书添加成功", "添加成功");
-      };
-      var fail_cbk = function (result) {
-        App.app.hideIndicator();
-        App.app.alert("图书添加失败", "添加失败");
-      };
-        
-      data.addThisBook = function() {
-        App.app.showIndicator();
-        bookmanage.addBook(data.isbn, bookjson, succ_cbk, fail_cbk);
-      };
+      ts.isbn = data.isbn;
+      ts.bookjson = data.bookjson;
       ts.$el.html(ts.template(data))
     });
-  }
+  },
+  events: {
+      'click #addbook' : 'addThisBook'
+  },
+  addThisBook: function() {
+        var ts = this;
+        var succ_cbk = function (result) {
+            App.app.hideIndicator();
+            App.app.alert("图书添加成功", "添加成功");
+        };
+        var fail_cbk = function (result) {
+            App.app.hideIndicator();
+            App.app.alert("图书添加失败", "添加失败");
+        };
+        App.app.showIndicator();
+        bookmanage.addBook(ts.isbn, ts.bookjson, succ_cbk, fail_cbk);
+    }
+
 })
 
 module.exports = BookView;
